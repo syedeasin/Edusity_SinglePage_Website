33439457dce347bf5178e45ffcb9b1636752382a
syedeasin <syedeasin1000@gmail.com>
2024-08-13 09:48:09 +0600

    Programs Section Desktop Version is complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Programs from "./components/Programs/Programs";
+import Title from "./components/Title/Title";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
-      <Programs />
+      <div className="container">
+        <Title subTitle="Our PROGRAM" title="What We Offer" />
+        <Programs />
+      </div>
     </div>
   );
 };
